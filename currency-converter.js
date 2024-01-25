c41891a2d391a2f2e194eb84a43f1556ab634584
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-01-24 22:04:51 -0500

    Fix getExchangeRate()

--- currency-converter.js
+++ currency-converter.js
@@ -2,10 +2,12 @@ const axios = require('axios');
 
 const getExchangeRate = (fromCurrency,toCurrency) =>{
 	axios.get('http://apilayer.net/api/live?access_key=268c2fed9a2e56c9bf97fa07ff916376&format').then((response) =>{
-		const rate = response.data.rates;
-		console.log(response.data);
+		const rate = response.data.quotes;
+		const exchangeRate = rate[`${fromCurrency}${toCurrency}`]
+		console.log(exchangeRate);
 	});
 }
 
 
-getExchangeRate('USD','EUR')
+getExchangeRate('USD','COP')
+
